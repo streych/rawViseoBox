f392f66a42c8aaa820111a720663fac1db759c7d
V radiulov <radiulovv.a@gmail.com>
2021-08-01 09:06:21 +1000

    add binding

--- app/src/main/java/com/example/raw_videobox/MainActivity.kt
+++ app/src/main/java/com/example/raw_videobox/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.raw_videobox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.raw_videobox.databinding.MainActivityBinding
 import com.example.raw_videobox.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(binding.container.id, MainFragment.newInstance())
                 .commitNow()
         }
     }
--- app/src/main/java/com/example/raw_videobox/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/raw_videobox/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.raw_videobox.R
+import com.example.raw_videobox.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,18 +16,25 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private var _binding: MainFragmentBinding? = null
+    private val binding
+    get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
